0c9a30c9c3aded86ac3101776d76f99c72820359
CrappyDill <mddillon47@yahoo.com>
2016-06-26 14:33:18 -0700

    update

--- dungeon.rb
+++ dungeon.rb
@@ -1,9 +1,10 @@
 class Dungeon
-	attr_accessor :player
+	attr_accessor :player, :weapon
 	
 	def initialize(player_name)
 		@player = Player.new(player_name)
 		@rooms = []
+		@weapons = []
 	end
 	
 	def add_room(reference, name, description, connections)
@@ -13,6 +14,11 @@ class Dungeon
 	def start(location)
 		@player.location = location
 		show_current_description
+	end
+	
+	def start_weapon(weapon)
+		@player.weapon = weapon
+		show_current_weapon
 	end
 	
 	def show_current_description
@@ -33,14 +39,24 @@ class Dungeon
 		show_current_description
 	end
 	
+	def add_weapon(referencew, namew, descriptionw)
+		@weapons << Weapon.new(referencew, namew, descriptionw)
+	end
+	
+	def find_weapon_in_dungeon(referencew)
+		@weapons.detect { |weapon| weapon.referencew == referencew }
+	end
+	
+	def show_current_weapon
+		find_weapon_in_dungeon(@player.weapon).weapon_description
+	end
+	
 	class Player
-		attr_accessor :name, :location
+		attr_accessor :name, :location, :weapon
 		
 		def initialize(name)
 			@name = name
 		end
-		
-		
 	end
 	
 	class Room 
@@ -57,18 +73,31 @@ class Dungeon
 			@name + "\n\nYou are in " + @description
 		end
 	end
+	
+	class Weapon
+		attr_accessor :referencew, :namew, :descriptionw
+			
+		def initialize(referencew, namew, descriptionw)
+			@referencew = referencew
+			@namew = namew
+			@descriptionw = descriptionw
+		end
+				
+		def weapon_description
+			"You have a " + @namew + " with " + @descriptionw
+		end
+	end
 end
 
 my_dungeon = Dungeon.new("Matty")
 my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", {:west => :smallcave, :south => :cavepond})
 my_dungeon.add_room(:smallcave, "Small Cave", "a small, claustrophobic cave", {:east => :largecave})
 my_dungeon.add_room(:cavepond, "Cave Pond", "a cave pond", {:south => :smallcave})
+my_dungeon.add_weapon(:sword, "Basic Sword", "1 Strength")
+my_dungeon.add_weapon(:bow, "Bow and Arrow", "4 Strenght")
 
-
+my_dungeon.start_weapon(:sword)
 my_dungeon.start(:smallcave)
 my_dungeon.go(:east)
-my_dungeon.go(:south)
 
-my_dungeon.show_current_description
-
-
+my_dungeon.show_current_weapon
